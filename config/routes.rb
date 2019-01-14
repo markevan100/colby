d94f505046fcfc4650b76d67adb0283e43b38d9e
Mark Modak <mmodak@mmodak.local>
2019-01-13 21:07:01 -0700

    Added a root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
   get 'welcome/home', 'welcome#home'
   get 'welcome/poem', 'welcome#poem'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
